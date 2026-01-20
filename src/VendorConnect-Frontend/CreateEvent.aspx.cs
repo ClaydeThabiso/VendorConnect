59160b296d5e0e0e1baf8e189063ed0367cbc45f
ClaydeThabiso <thabisoclayde@gmail.com>
2026-01-20 14:18:55 +0200

    Update CreateEvent.asp.cs

--- src/VendorConnect-Frontend/CreateEvent.aspx.cs
+++ src/VendorConnect-Frontend/CreateEvent.aspx.cs
@@ -12,6 +12,12 @@ namespace VendorConnect_Frontend
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["UserID"] == null || (char)Session["LoggedIn"] != 'O')
+            {
+                Response.Redirect("Login.aspx");
+                return;
+            }
+
             if (!IsPostBack)
             {
                 Service1Client client = new Service1Client();
