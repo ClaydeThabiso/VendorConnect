9391d5f2c30ba230a1958cd23935263bfe23925b
ClaydeThabiso <thabisoclayde@gmail.com>
2026-01-20 14:17:11 +0200

    Update AdminOrganizerReport.aspx.cs

--- src/VendorConnect-Frontend/AdminOrganizerReport.aspx.cs
+++ src/VendorConnect-Frontend/AdminOrganizerReport.aspx.cs
@@ -12,7 +12,13 @@ namespace VendorConnect_Frontend
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(!IsPostBack)
+            if (Session["UserID"] == null || (char)Session["LoggedIn"] != 'A')
+            {
+                Response.Redirect("Login.aspx");
+                return;
+            }
+
+            if (!IsPostBack)
             {
                 LoadOrganizerReport();
             }
