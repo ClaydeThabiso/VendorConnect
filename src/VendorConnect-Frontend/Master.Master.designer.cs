8d69156258f8af9adfa86f1e21835d66c86bcf51
ClaydeThabiso <thabisoclayde@gmail.com>
2026-02-03 19:03:07 +0200

    update master

--- src/VendorConnect-Frontend/Master.Master.designer.cs
+++ src/VendorConnect-Frontend/Master.Master.designer.cs
@@ -42,24 +42,6 @@ namespace VendorConnect_Frontend
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// notifCount control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl notifCount;
-
-        /// <summary>
-        /// RepeaterNotifications control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater RepeaterNotifications;
-
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
