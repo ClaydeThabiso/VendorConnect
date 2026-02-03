5b00feca92d0997fbc84482b15b03fd722cbd6b9
ClaydeThabiso <thabisoclayde@gmail.com>
2026-02-03 19:07:53 +0200

    Update VendorDashboard.aspx.cs

--- src/VendorConnect-Frontend/VendorDashboard.aspx.cs
+++ src/VendorConnect-Frontend/VendorDashboard.aspx.cs
@@ -21,7 +21,7 @@ namespace VendorConnect_Frontend
             if (!IsPostBack)
             {
                 Service1Client client = new Service1Client();
-
+                LoadNotifications();
                 int userID = Convert.ToInt32(Session["UserID"]);
                 var u = client.GetUser(userID);
                 if(u!=null)
@@ -70,6 +70,47 @@ namespace VendorConnect_Frontend
                 client.Close();
             }
 
+        }
+        private void LoadNotifications()
+        {
+            int userId = Convert.ToInt32(Session["UserID"]);
+
+            using (Service1Client client = new Service1Client())
+            {
+                dynamic notifications = client.GetUserNotifications(userId);
+                var unreadCount = client.GetUnreadNotificationCount(userId);
+
+                RepeaterNotifications.DataSource = notifications;
+                RepeaterNotifications.DataBind();
+
+                if (unreadCount > 0)
+                {
+                    notifCount.Visible = true;
+                    notifCount.InnerText = unreadCount.ToString();
+                }
+                else
+                {
+                    notifCount.Visible = false;
+                }
+            }
+        }
+        protected void RepeaterNotifications_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            if (e.CommandName == "Open")
+            {
+                string[] args = e.CommandArgument.ToString().Split('|');
+
+                int notificationId = Convert.ToInt32(args[0]);
+                string redirectUrl = args.Length > 1 ? args[1] : null;
+
+                using (Service1Client client = new Service1Client())
+                {
+                    client.MarkNotificationAsRead(notificationId);
+                }
+
+                if (!string.IsNullOrEmpty(redirectUrl))
+                    Response.Redirect(redirectUrl);
+            }
         }
     }
 }
