8efc276986415ede609e4856260c1a5f7b2bdac5
ClaydeThabiso <thabisoclayde@gmail.com>
2026-02-03 16:47:09 +0200

    Update Master.Master.cs

--- src/VendorConnect-Frontend/Master.Master.cs
+++ src/VendorConnect-Frontend/Master.Master.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VendorConnect_Frontend.ServiceReference1;
 
 namespace VendorConnect_Frontend
 {
@@ -16,5 +17,48 @@ namespace VendorConnect_Frontend
                 LoadNotifications();
             }
         }
+        private void LoadNotifications()
+        {
+            int userId = Convert.ToInt32(Session["UserID"]);
+
+            using (Service1Client client = new Service1Client())
+            {
+                var notifications = client.GetUserNotifications(userId);
+                var unreadCount = client.GetUnreadNotificationCount(userId);
+
+                RepeaterNotifications.DataSource = notifications;
+                RepeaterNotifications.DataBind();
+
+                if (unreadCount > 0)
+                {
+                    notifCount.Visible = true;
+                    notifCount.InnerText = unreadCount.ToString();
+                }
+                else
+                {
+                    notifCount.Visible = false;
+                }
+            }
+        }
+        protected void RepeaterNotifications_ItemCommand(object source,RepeaterCommandEventArgs e)
+        {
+            if (e.CommandName == "Open")
+            {
+                string[] args = e.CommandArgument.ToString().Split('|');
+
+                int notificationId = Convert.ToInt32(args[0]);
+                string redirectUrl = args.Length > 1 ? args[1] : null;
+
+                using (Service1Client client = new Service1Client())
+                {
+                    client.MarkNotificationAsRead(notificationId);
+                }
+
+                if (!string.IsNullOrEmpty(redirectUrl))
+                    Response.Redirect(redirectUrl);
+            }
+        }
+
+
     }
 }
