5ec07c674e27acea91f763884b1633a83860c7e5
ClaydeThabiso <thabisoclayde@gmail.com>
2026-02-07 16:06:01 +0200

    modify vendorNotification.aspx

--- src/VendorConnect-Frontend/VendorNotifications.aspx.designer.cs
+++ src/VendorConnect-Frontend/VendorNotifications.aspx.designer.cs
@@ -40,5 +40,14 @@ namespace VendorConnect_Frontend
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater RepeaterNotifications;
+
+        /// <summary>
+        /// pnlNoNotifications control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlNoNotifications;
     }
 }
